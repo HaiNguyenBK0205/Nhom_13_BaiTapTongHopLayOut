73e99bec994c431f2b70057804b87da9f15d1502
HaiNguyenBK0205 <minhhai123yn@gmail.com>
2021-01-25 03:29:32 +0700

    add homeTool - Thanh dat ve xem phim

--- js/hainguyen.js
+++ js/hainguyen.js
@@ -9,3 +9,23 @@ function close_side_panel() {
     document.getElementById("after-dn").style.display = "none";
     document.getElementById("side-panel-bg").style.display = "none";
 }
+
+// homeTool
+function homeTool_Opendropdown(x) {
+    var homeTool_menu_list = document.getElementsByClassName("homeTool-menu-item");
+    for (var i = 0; i < homeTool_menu_list.length; i++) {
+        homeTool_menu_list[i].classList.remove("show");
+    }
+    homeTool_menu_list[x].classList.add("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.homeTool-button')) {
+        var homeTool_menu_list = document.getElementsByClassName("homeTool-menu-item");
+        for (var i = 0; i < homeTool_menu_list.length; i++) {
+            var openDropdown = homeTool_menu_list[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
